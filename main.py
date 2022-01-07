0c71fc8ec64d61b4c608678b3e24321bed3d3de8
nbbosco <nickilu009@gmail.com>
2022-01-06 23:41:26 -0300

    NB_Arch

--- main.py
+++ main.py
@@ -109,13 +109,13 @@ async def main():
     <h3 style="font-family:Arial">We'll Try to Predict Which of These Categories They Are:</h3><br>
     """
     original_paths = ['0.jpg', '1.jpg', '2.jpg', 
-                      '3.jpg', '4.jpg', '5.jpg',
-                      '6.jpg', '7.jpg', '8.jpg',
-                      '9.jpg', '10.jpg', '11.jpg',
-                      '12.jpg', '13.jpg', '14.jpg',
+                      '3.jpg', '4.jpg', '5.JPG',
+                      '6.JPG', '7.JPG', '8.jpg',
+                      '9.jpg', '10.JPG', '11.JPG',
+                      '12.jpg', '13.jpg', '14.JPG',
                       '15.jpg', '16.jpg', '17.jpg',
-                      '18.jpg', '19.jpg', '20.jpg',
-                      '21.jpg', '22.jpg', '23.jpg',
+                      '18.jpg', '19.JPG', '20.jpg',
+                      '21.jpg', '22.jpg', '23.JPG',
                       '24.jpg']
 
     full_original_paths = ['static/original/' + x for x in original_paths]
